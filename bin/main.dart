a8ddb1f3051c316844cbb1b50e1c314b6073d684
Kedirbiruh <kedirbiruh1114@gmail.com>
2025-09-23 23:54:13 +0200

    methode erstellt

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,11 @@
 import 'package:oop1/teilnehmer.dart' as oop1;
 
-void main {
-  var t1 = Teilnehmer("Abel", 35, geburtstagsdatum: DateTime(1990, 4, 23))
+void main () {
+  var t1 = oop1.Teilnehmer("Abel", 35, gewicht: 80.0, groesse: 1.70, status: "aktiv");
+  var t2 = oop1.Teilnehmer("Sahra", 19, groesse: 1.75, );
+
+  t1.info();
+  print("----");
+  t2.info();
+  print("--");
 }
