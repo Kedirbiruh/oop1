95ad3ebf9d76cc1a42ca7002398fef08a5e041ff
Kedirbiruh <kedirbiruh1114@gmail.com>
2025-09-26 12:30:49 +0200

    added toString.padLeft

--- lib/participant.dart
+++ lib/participant.dart
@@ -19,13 +19,13 @@ class Participant {
     this.weight,
     this.height,
     this.status = 'Aktiv',
-  }){
-
-  // Note prüfen
-  if (finalGrade != null && (finalGrade ! < 0 || finalGrade ! > 6)) {
-    throw ArgumentError('Note muss zwischen 0 und 6 liegen');
+  }) {
+    // Note prüfen
+    if (finalGrade != null && (finalGrade! < 0 || finalGrade! > 6)) {
+      throw ArgumentError('Note muss zwischen 0 und 6 liegen');
+    }
+    finalGrade = finalGrade;
   }
-    finalGrade = finalGrade;}
 
   // Beispiel-Setter mit Validierung (optional)
   set grade(double? value) {
@@ -35,26 +35,30 @@ class Participant {
     finalGrade = value;
   }
 
-    // Getter: zusammengesetzter Wert
+  // Getter: zusammengesetzter Wert
   String get fullName => '$firstName $lastName';
 
   void printInfo() {
     print('Name: $fullName');
     print('Status: $status');
     print('Alter: $age');
-    if (birthDate!= null) { // ist nicht keer / existiert ein Wert
-      print('Geburtstag: ${birthDate!.day}.${birthDate!.month}.${birthDate!.year}');
-       // Wenn birthDate einen Wert hat → das Datum
-      } else {
-        print('Geburtstag: nicht angegeben'); // Wenn birthDate null ist → "nicht angegeben"
+    if (birthDate != null) {
+      // ist nicht keer / existiert ein Wert
+      print(
+        'Geburtstag: ${birthDate!.day.toString().padLeft(2, '0')}.${birthDate!.month.toString().padLeft(2, '0')}.${birthDate!.year}',
+      );
+      // Wenn birthDate einen Wert hat → das Datum
+    } else {
+      print(
+        'Geburtstag: nicht angegeben',
+      ); // Wenn birthDate null ist → "nicht angegeben"
     }
-    if (finalGrade!= null) {
-      print('Abschlussnote: ${finalGrade!}'); 
+    if (finalGrade != null) {
+      print('Abschlussnote: ${finalGrade!}');
     } else {
       print('Abschlussnote: nicht vorhanden');
-    } 
+    }
   }
-
 
   @override
   String toString() {
